018382993b36740581ac586720cd2874b1dd954b
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2023-04-05 08:49:55 +0200

    add client library

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { ServerFeatureTodoModule } from '@fst/server/feature-todo'
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [ServerFeatureTodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
